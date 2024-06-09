5b63b393ed2c76bd8659f8373b8b687188f687be
Okky Rangga Pratama <echo.ikoyozu@gmail.com>
2024-06-09 21:03:28 +0700

    Modified All

--- config/auth.php
+++ config/auth.php
@@ -40,10 +40,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'admin' => [
-            'driver' => 'session',
-            'provider' => 'admins',
-        ],
     ],
 
     /*
@@ -65,10 +61,6 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
-        ],
-        'admins' => [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
